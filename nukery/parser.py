20cc939268f50a555640b669863b2f1d0bd974a5
rasheedgm <gm.abdul@gmail.com>
2024-04-03 16:27:48 +0530

    knob pasrse seperated

--- nukery/parser.py
+++ nukery/parser.py
@@ -45,40 +45,8 @@ class NukeScriptParser:
             last_node_content_start = match.start(2)
 
         for node_class, node_content, stack_statement in node_data:
-            knobs = []
-            inputs = ""
-            user_knobs = []
-            last_knob = None
-            last_knob_line_end = last_knob_value_start = None
-            if node_content is None:
-                node_content = ""
-            for knob_match in re.finditer(cls.knob_pattern, node_content, re.MULTILINE):
-                _spaces, knob_name, knob_value = knob_match.groups()
-                this_knob_line_start = knob_match.start()
 
-                if knob_name == "addUserKnob":
-                    user_knob_match = re.match(cls.user_knob_pattern, knob_value)
-                    knob_id = user_knob_name = ""
-                    if user_knob_match:
-                        knob_id, user_knob_name = user_knob_match.groups()
-                    user_knobs.append((user_knob_name, knob_id, knob_value))
-                else:
-                    if knobs:
-                        if last_knob_line_end + 1 != this_knob_line_start:
-                            last_knob_correct_value = node_content[last_knob_value_start:this_knob_line_start]
-                            knobs.pop(-1)
-                            knobs.append((last_knob, last_knob_correct_value))
-                    else:
-                        # if first knob script is inputs then its note knob, but inputs number
-                        if knob_name == "inputs":
-                            inputs = knob_value
-                            continue
-
-                    knobs.append((knob_name, knob_value))
-
-                last_knob = knob_name
-                last_knob_line_end = knob_match.end()
-                last_knob_value_start = this_knob_line_start + len(_spaces) + len(knob_name)
+            knobs, user_knobs, inputs = cls.parse_knob_script(node_content)
 
             type_ = var = stack_index = None
             if not knobs:
@@ -110,6 +78,45 @@ class NukeScriptParser:
                 "stack_index": stack_index,
                 "node_content": node_content
             }
+
+    @classmethod
+    def parse_knob_script(cls, node_content):
+        knobs = []
+        inputs = ""
+        user_knobs = []
+        last_knob = None
+        last_knob_line_end = last_knob_value_start = None
+        if node_content is None:
+            node_content = ""
+        for knob_match in re.finditer(cls.knob_pattern, node_content, re.MULTILINE):
+            _spaces, knob_name, knob_value = knob_match.groups()
+            this_knob_line_start = knob_match.start()
+
+            if knob_name == "addUserKnob":
+                user_knob_match = re.match(cls.user_knob_pattern, knob_value)
+                knob_id = user_knob_name = ""
+                if user_knob_match:
+                    knob_id, user_knob_name = user_knob_match.groups()
+                user_knobs.append((user_knob_name, knob_id, knob_value))
+            else:
+                if knobs:
+                    if last_knob_line_end + 1 != this_knob_line_start:
+                        last_knob_correct_value = node_content[last_knob_value_start:this_knob_line_start]
+                        knobs.pop(-1)
+                        knobs.append((last_knob, last_knob_correct_value))
+                else:
+                    # if first knob script is inputs then its note knob, but inputs number
+                    if knob_name == "inputs":
+                        inputs = knob_value
+                        continue
+
+                knobs.append((knob_name, knob_value))
+
+            last_knob = knob_name
+            last_knob_line_end = knob_match.end()
+            last_knob_value_start = this_knob_line_start + len(_spaces) + len(knob_name)
+
+        return knobs, user_knobs, inputs
 
     @classmethod
     def from_text(cls, text):
